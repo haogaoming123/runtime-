10a3e128be439836eaa723817b9ebf7c5ed6e426
郝高明 <978235424@qq.com>
2017-02-16 17:34:04 +0800

    动态创建类，添加方法，添加属性，替换方法

--- runtime01/main.m
+++ runtime01/main.m
@@ -25,6 +25,9 @@ void TestMetaClass(id self, SEL _cmd) {
     NSLog(@"NSObject is meta class is %p",objc_getClass((__bridge void *)[NSObject class]));
 }
 
+void imp_submethod1(id self,SEL _cmd,NSNumber* index) {
+    NSLog(@"run sub method %ld",(long)index.integerValue);
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -146,6 +149,32 @@ int main(int argc, const char * argv[]) {
         }
         
         NSLog(@"==============================================");
+        
+        Class clss = objc_allocateClassPair(myclass.class, "MySubClass", 0);
+        //添加方法
+        class_addMethod(clss, @selector(method3), (IMP)imp_submethod1, "v@:");
+        //替换方法
+        class_replaceMethod(clss, @selector(method1), (IMP)imp_submethod1, "v@:");
+        //添加属性
+        //属性类型  name值：T  value：变化
+        //编码类型  name值：C(copy) &(strong) W(weak) 空(assign) 等 value：无
+        //非/原子性 name值：空(atomic) N(Nonatomic)  value：无
+        //变量名称  name值：V  value：变化
+//        objc_property_attribute_t type =
+        objc_registerClassPair(clss);
+        
+        id mySubClass = [[clss alloc] init];
+        [mySubClass performSelector:@selector(method3) withObject:@(123)];
+        [mySubClass performSelector:@selector(method1) withObject:@(321)];
+        
+        objc_property_t *mysubClassIvar = class_copyPropertyList(clss, &outCount);
+        for (int i=0; i<outCount; i++) {
+            objc_property_t ivar = mysubClassIvar[i];
+            NSLog(@"MySubClass's property is : %s",property_getName(ivar));
+        }
+        free(mysubClassIvar);
+        
+        NSLog(@"=========================================");
     }
     return 0;
 }
